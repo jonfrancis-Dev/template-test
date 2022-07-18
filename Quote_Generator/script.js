78184961065be1dca39cdaee18959d84064feb00
Jonathan Francis <jonfrancis.dev@gmail.com>
2022-07-18 19:19:07 -0400

    added spinning loader

--- Quote_Generator/script.js
+++ Quote_Generator/script.js
@@ -3,13 +3,26 @@ const quoteText =document.getElementById('quote');
 const authorText =document.getElementById('author');
 const twitterBtn =document.getElementById('twitter');
 const newQuoteBtn =document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
 let apiQuotes = [];
 
+
+//Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Hide Loader
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 //Show New Quote
 function newQuote() {
+    loading();
     //Pick Random Quote from apiQuotes Array
     const quote =apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if Author Field is Blank
@@ -25,12 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    //Set Quote, Hide Loader
     quoteText.textContent =quote.text;
+    complete();
 }
 
 //Get quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
